49f2989f96e5ef05ca468a98cfabc60d9fbdd5b4
Bawazier <32612909+Bawazier@users.noreply.github.com>
2021-05-20 02:15:11 +0700

    update responsive header

--- components/Header.js
+++ components/Header.js
@@ -1,32 +1,69 @@
 import React from "react";
-import Link from "next/link";
 import {
   Box,
+  Container,
   Flex,
   Spacer,
   HStack,
-  Link as ChakraLink,
+  Button,
   Heading,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  IconButton,
 } from "@chakra-ui/react";
-// import {FaPlus} from "react-icons/fa";
+import {FaPlus} from "react-icons/fa";
+import { MdReorder } from "react-icons/md";
 
 function Header() {
   return (
     <Box bg="gray.500" color="white" py="4" px="8">
-      <Flex>
-        <Heading as="h2" size="xl">
-          NavBar Brand
-        </Heading>
-        <Spacer />
-        <HStack spacing="28px">
-          <ChakraLink as={Link} href="#" size="lg">
-            Add Event
-          </ChakraLink>
-          <ChakraLink as={Link} href="#" size="lg">
-            Dashboard
-          </ChakraLink>
-        </HStack>
-      </Flex>
+      <Container maxW="container.xl">
+        <Flex>
+          <Heading as="h2" size="xl">
+            NavBar Brand
+          </Heading>
+          <Spacer />
+          <HStack
+            display={["none", "flex"]}
+            spacing="28px"
+            fontSize="18px"
+            fontWeight="bold"
+          >
+            <Button
+              colorScheme="whiteAlpha"
+              color="white"
+              leftIcon={<FaPlus />}
+              variant="link"
+              size="lg"
+            >
+              Add Event
+            </Button>
+            <Button
+              colorScheme="whiteAlpha"
+              color="white"
+              variant="link"
+              size="lg"
+            >
+              Dashboard
+            </Button>
+          </HStack>
+          <Menu display={["block", "none"]}>
+            <MenuButton
+              as={IconButton}
+              aria-label="Options"
+              icon={<MdReorder />}
+              variant="outline"
+              display={["flex", "none"]}
+            />
+            <MenuList>
+              <MenuItem icon={<FaPlus />}>Add Event</MenuItem>
+              <MenuItem>Dashboard</MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+      </Container>
     </Box>
   );
 }
