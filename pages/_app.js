0663fe46c268256d0fe661b9bdf3a102c46c4442
Bawazier <32612909+Bawazier@users.noreply.github.com>
2021-05-20 16:18:09 +0700

    custom app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+/* eslint-disable react/prop-types */
+import React from "react";
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { Hydrate } from "react-query/hydration";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const queryClientRef = React.useRef();
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
+  return (
+    <ChakraProvider>
+      <QueryClientProvider client={queryClientRef.current}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
