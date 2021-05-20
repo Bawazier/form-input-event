c456c458c13ef2db355bafb05ee61f4b5247f812
Bawazier <32612909+Bawazier@users.noreply.github.com>
2021-05-20 13:10:03 +0700

    update link page

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import {
   Box,
   Container,
@@ -17,13 +19,17 @@ import {FaPlus} from "react-icons/fa";
 import { MdReorder } from "react-icons/md";
 
 function Header() {
+  const router = useRouter();
   return (
     <Box bg="gray.500" color="white" py="4" px="8">
       <Container maxW="container.xl">
         <Flex>
-          <Heading as="h2" size="xl">
-            NavBar Brand
-          </Heading>
+          <Link href="/">
+            <Heading as="h2" size="xl" cursor="pointer">
+              NavBar Brand
+            </Heading>
+          </Link>
+
           <Spacer />
           <HStack
             display={["none", "flex"]}
@@ -37,6 +43,7 @@ function Header() {
               leftIcon={<FaPlus />}
               variant="link"
               size="lg"
+              onClick={() => router.push("/add-event")}
             >
               Add Event
             </Button>
@@ -45,6 +52,7 @@ function Header() {
               color="white"
               variant="link"
               size="lg"
+              onClick={() => router.push("/dashboard")}
             >
               Dashboard
             </Button>
@@ -58,8 +66,8 @@ function Header() {
               display={["flex", "none"]}
             />
             <MenuList>
-              <MenuItem icon={<FaPlus />}>Add Event</MenuItem>
-              <MenuItem>Dashboard</MenuItem>
+              <MenuItem onClick={() => router.push("/add-event")} icon={<FaPlus />}>Add Event</MenuItem>
+              <MenuItem onClick={() => router.push("/dashboard")}>Dashboard</MenuItem>
             </MenuList>
           </Menu>
         </Flex>
