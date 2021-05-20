c71705787de09864260900ad47d95b11b1c676fa
Bawazier <32612909+Bawazier@users.noreply.github.com>
2021-05-20 13:12:53 +0700

    update dynamic data

--- components/CardEvent.js
+++ components/CardEvent.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-key */
 import React from "react";
 import {
   Container,
@@ -10,7 +12,7 @@ import {
 import {MdLocationOn} from "react-icons/md";
 import {IoPersonCircle} from "react-icons/io5";
 
-function CardEvent() {
+function CardEvent({title, location, date, participant, note, thumbnail}) {
   return (
     <Container
       maxW="340px"
@@ -25,7 +27,7 @@ function CardEvent() {
         <Image
           boxSize="full"
           objectFit="cover"
-          src="https://via.placeholder.com/728x90.png/?text=Visit+WhoIsHostingThis.com+Buyers+Guide"
+          src={thumbnail}
           alt="Thumbnail"
         />
       </Box>
@@ -37,47 +39,34 @@ function CardEvent() {
             color="black"
             fontWeight="black"
             casing="uppercase"
+            noOfLines={1}
           >
-            Pisangan timur, jakarta
+            {location}
           </Text>
         </HStack>
         <VStack align="start" spacing="1px">
           <Text fontSize="3xl" fontWeight="hairline" maxW="full" isTruncated>
-            Meeting With CEO
+            {title}
           </Text>
           <Text color="gray.800" fontSize="14px" fontWeight="light">
-            17 Agustus 2020
+            {date}
           </Text>
         </VStack>
         <HStack shouldWrapChildren wrap="wrap">
-          <HStack>
-            <IoPersonCircle color="#3182ce" />
-            <Text fontSize="14px" color="gray.800" maxW="24" isTruncated>
-              Rio Jainadi lacus sed turpis tincidunt id aliquet risus feugiat
-            </Text>
-          </HStack>
-          <HStack>
-            <IoPersonCircle color="#3182ce" />
-            <Text fontSize="14px" color="gray.800" maxW="24" isTruncated>
-              Dimas P.
-            </Text>
-          </HStack>
-          <HStack>
-            <IoPersonCircle color="#3182ce" />
-            <Text fontSize="14px" color="gray.800" maxW="24" isTruncated>
-              Raditsian
-            </Text>
-          </HStack>
+          {participant?.map((item) => (
+            <HStack>
+              <IoPersonCircle color="#3182ce" />
+              <Text fontSize="14px" color="gray.800" maxW="20" isTruncated>
+                {item}
+              </Text>
+            </HStack>
+          ))}
         </HStack>
         <Box>
           <VStack align="start" spacing="2px">
             <Text fontWeight="bold">Note :</Text>
             <Text fontSize="14px" color="gray.800" noOfLines={4}>
-              lacus sed turpis tincidunt id aliquet risus feugiat in ante metus
-              dictum at tempor commodo ullamcorper a lacus vestibulum sed arcu
-              non odio euismod lacinia at quis risus sed vulputate odio ut enim
-              blandit volutpat maecenas volutpat blandit aliquam etiam erat
-              velit scelerisque in dictum non consectetur a erat nam
+              {note}
             </Text>
           </VStack>
         </Box>
